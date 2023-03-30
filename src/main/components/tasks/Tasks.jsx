680a6631c7ff34c49e809a8ebe16a3a3a435ff50
= <=>
2023-03-30 10:33:10 -0300

    [main] Add key component task

--- src/main/components/tasks/Tasks.jsx
+++ src/main/components/tasks/Tasks.jsx
@@ -4,7 +4,8 @@ import Task from "./task-item/Task";
 
 const Tasks = ({tasks, handleTaskCompleted, handleTaskRemover}) => {
     return (
-        <div>{tasks.map(task => <Task   task={task} 
+        <div>{tasks.map(task => <Task   key={task.id}
+                                        task={task} 
                                         handleTaskCompleted={handleTaskCompleted}
                                         handleTaskRemover={handleTaskRemover}/>)}</div>
     );
